9a41bf46d8156739d4a9a2f08a0647fe1af3dbbd
matiasqj <matias.quinteros@usach.cl>
2016-04-25 21:51:15 -0300

    Vista homepage

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -29,7 +29,9 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
+  
   'js/dependencies/**/*.js',
+  'js/materialize.min.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
