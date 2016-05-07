2acd23b5d023abf515eb4e0a3745895ee1d7c569
Matias Quinteros <matias.quinteros@usach.cl>
2016-05-06 22:27:56 -0300

    Varios cambios front end

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -73,5 +73,7 @@ module.exports.jsFilesToInject = jsFilesToInject.map(function(jsPath) {
 module.exports.templateFilesToInject = templateFilesToInject.map(function(tplPath) {
   return require('path').join('assets/',tplPath);
 });
-
+module.exports.templateFilesToInject = templateFilesToInject.map(function(tplPath) {
+  return require('path').join('assets/images/',tplPath);
+});
 
