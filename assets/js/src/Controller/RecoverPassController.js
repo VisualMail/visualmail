fbfb91ddae6a4c96b60dc7c70d59ddf68843a4d9
matiasqj <matias.quinteros@usach.cl>
2016-05-16 20:18:45 -0400

    Arreglos de usabiidad en vistas de recuperacion

--- assets/js/src/Controller/RecoverPassController.js
+++ assets/js/src/Controller/RecoverPassController.js
@@ -25,10 +25,18 @@ $http({
         }
 
     }).success(function (data) {
-    	$scope.mensaje1 = $('<span class="color_acentuado-text"> Se ha enviado un correo a '+$scope.email+' </span>');
-    	 Materialize.toast($scope.mensaje1, 5000);
-    	 $scope.mostrar=false;
-    	 $scope.mostrarpost=true;
+    	   if(data.opcion=='true'){
+              $scope.mensaje1 = $('<span class="color_acentuado-text"> Se ha enviado un correo a '+$scope.email+' </span>');
+             Materialize.toast($scope.mensaje1, 5000);
+             $scope.mostrar=false;
+             $scope.mostrarpost=true;
+           }
+           else{
+              $scope.mensaje2 = $('<span class="color_acentuado-text"> Ocurrió un error, correo no existe </span>');
+              Materialize.toast($scope.mensaje2, 5000);
+           }   
+    	
+    	
     });
 
 	}//fin senddata
