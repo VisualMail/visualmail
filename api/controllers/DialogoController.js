41e230ad08341711e7aea6077768b35d1f6656d3
matiasqj <matias.quinteros@usach.cl>
2016-05-26 22:46:19 -0400

    arreglos de vistas

---
+++ api/controllers/DialogoController.js
@@ -0,0 +1,11 @@
+/**
+ * DialogoController
+ *
+ * @description :: Server-side logic for managing dialogoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
