fc144c84cfdd617cef6e398f49841a2e3b3a31d6
matiasqj <matias.quinteros@usach.cl>
2016-06-27 21:57:14 -0400

    hok

--- config/email.js
+++ config/email.js
@@ -1,22 +1,13 @@
 module.exports.email = {
-service: 'Gmail',
-
-         transporter: {
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'noreply.visualmail@gmail.com',
-                pass: 'visualmail2016'
-            },
-           // tls: {rejectUnauthorized: false},
-            //debug:true          
-        },
- 	templateDir:  'views/emailTemplates',
-   	from: 'noreply@visualmail.com',
-    testMode: false,
-     ssl: true
- //ssl: true
+service: 'Mailgun',
+ auth: {
+ user: 'postmaster@app861ea74e5a0e4900a1ede12f23e2e12b.mailgun.org', 
+ pass: 'visualmail2016'
+ },
+ templateDir: 'views/emailTemplates',
+ from: 'noreply.visualmail@gmail.com',
+ testMode: false,
+ ssl: true
 
  /*service: 'Mailgun',
  auth: {
