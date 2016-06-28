90c2219133d1bb397f82d7fc03199c16f619765c
matiasqj <matias.quinteros@usach.cl>
2016-06-27 21:28:01 -0400

    hok

--- config/email.js
+++ config/email.js
@@ -20,5 +20,16 @@ service: 'Gmail',
     testMode: false,
      ssl: true
  //ssl: true
+
+ /*service: 'Mailgun',
+ auth: {
+ user: 'postmaster@app861ea74e5a0e4900a1ede12f23e2e12b.mailgun.org', 
+ pass: 'visualmail2016'
+ },
+ templateDir: 'views/emailTemplates',
+ from: 'noreply.visualmail@gmail.com',
+ testMode: false,
+ ssl: true*/
+
 }
 
