69df0ab1b59b775cf9674fe9def2a5c934e16b60
Santiago <sbraganzab@users.noreply.github.com>
2017-01-13 11:32:12 -0300

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -25,14 +25,14 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-   port: process.env.PORT || 80,
+   port: process.env.PORT || 1337,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
 
-  // log: {
-  //   level: "silent"
-  // }
+  log: {
+     level: "verbose"
+  }
 
 };
