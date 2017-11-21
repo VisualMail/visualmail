533e972420c495988af0654c5453a96f945d7afb
sbraganzab <santiago.braganza@usach.cl>
2017-11-21 18:43:58 -0300

    Ok

--- config/connections.js
+++ config/connections.js
@@ -59,7 +59,7 @@ module.exports.connections = {
 
   someMongodbServer: { 
     adapter: "sails-mongo", 
-    url: "mongodb://visualmail:visualmail2017@ds053216.mlab.com:53216/visualmaildb" 
+    url: "mongodb://visualmaildb:visualmail2017@ds117116-a0.mlab.com:17116,ds117116-a1.mlab.com:17116/visualmaildb?replicaSet=rs-ds117116" 
   },
   
   /*someMongodbServer: { 
